5f2e268fb3d23be4d583890e54580e66427d1816
KerimliVusal <kerimlivusal98@gmail.com>
2023-10-06 01:23:04 +0400

    adding structure and styles

---
+++ src/components/Input/index.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { Input } from 'antd';
+import { CheckCircleFilled, CloseCircleFilled } from '@ant-design/icons'; 
+import '../../App.css'
+
+const Inputs = ({error,onChange,value,mount,Mount,data}) => {
+    useEffect(()=>{
+        if(error){Mount(true)}
+    },[error])
+  return (
+    <div className={`Input ${(mount||data )&&(!error? 'inputCorrect' :'inputError')}`}>
+        <Input onChange={onChange} value={value}  className='custom-input-class' />
+        {(mount||data)&&(!error?<CheckCircleFilled className='correct'/>: <CloseCircleFilled className='error'/>)}
+        {error?<span className='erorrText'>{error.message}</span>:''}
+    </div>
+  );
+};
+
+export default Inputs;
---
+++ src/containers/Form/index.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { useForm, FormProvider, Controller } from 'react-hook-form';
+import { Form, Button, Row, Col, Space } from 'antd';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMount } from 'ahooks'
+import '../../App.css'
+import Inputs from '../../components/Input';
+
+const SubmitForm = () => {
+    const [data, setData] = useState(false)
+    const [mount, Mount] = useState(false)
+    // Yup schema for form validation
+    const schema = yup.object().shape({
+        firstName: yup.string().required('First Name is required'),
+        lastName: yup.string().required('Last Name is required'),
+        email: yup.string().email('Invalid email address').required('Email is required'),
+        phoneNumber: yup.string().required('Phone Number is required').matches(/^\d+$/, 'Invalid Phone Number'),
+    });
+    useMount(() => {
+        methods.clearErrors()
+    });
+    // react-hook-form setup
+    const methods = useForm({
+        mode: "onSubmit",
+        resolver: yupResolver(schema),
+    });
+
+    // Form submission handler
+    const onSubmit = (data) => {
+        setData(true)
+    };
+    const handleReset=()=>{
+        methods.clearErrors()
+        methods.reset()
+        setData(false)
+        Mount(false)
+
+    }
+    return (
+        <FormProvider  {...methods}>
+            {mount&&!data?<span className='errorMessage'><p>One of the fields is incorrect or invalid.Please follow the example in order to continue</p></span>:''
+}
+            <form onSubmit={methods.handleSubmit(onSubmit)} style={{ width: '600px', margin: 'auto', padding: '10px 30px 10px 50px' }} className='formContainer'>
+                <Row gutter={16}>
+                    <Col span={24}>
+                        <Col span={24} style={{ textAlign: 'center' }}>
+                            <h1>Form</h1>
+                        </Col>
+                        <Col span={24}>
+                            <Form.Item
+                                label="First Name"
+                            >
+                                <Controller control={methods.control} name='firstName' render={({ field: { onChange, value }, fieldState: { error } }) => (<Inputs onChange={onChange} error={error} mount={mount} Mount={Mount} data={data} value={value} />)} />
+                            </Form.Item>
+                        </Col>
+                        <Col span={24}>
+                            <Form.Item
+                                label="Last Name"
+                            >
+                                <Controller control={methods.control} name='lastName' render={({ field: { onChange, value }, fieldState: { error } }) => (<Inputs onChange={onChange} error={error} mount={mount} Mount={Mount} data={data} value={value} />)} />
+                            </Form.Item>
+                        </Col>
+                        <Col span={24}>
+                            <Form.Item
+                                label="Email"
+                            >
+                                <Controller control={methods.control} name='email' render={({ field: { onChange, value }, fieldState: { error } }) => (<Inputs onChange={onChange} error={error} mount={mount} Mount={Mount} data={data} value={value} />)} />
+                            </Form.Item>
+                        </Col>
+                        <Col span={24}>
+                            <Form.Item
+                                label="Phone Number"
+                            >
+                                <Controller control={methods.control} name='phoneNumber' render={({ field: { onChange, value }, fieldState: { error } }) => (<Inputs onChange={onChange} error={error} mount={mount} Mount={Mount} data={data} value={value} />)} />
+                            </Form.Item>
+                        </Col>
+
+                        <Col span={24} style={{ textAlign: 'right' ,marginRight:'30px'}}>
+                            {!data ?
+                                <Form.Item>
+                                    <Button type="primary" size="large" htmlType="submit" loading={methods.loading}>
+                                        Submit
+                                    </Button>
+                                </Form.Item>
+                                :
+                                <Form.Item>
+                                    <Button type="primary" size="large" onClick={handleReset} loading={methods.loading}>
+                                        Reset
+                                    </Button>
+                                </Form.Item>
+                            }
+                        </Col>
+                    </Col>
+                </Row>
+            </form>
+        </FormProvider>
+    );
+};
+
+export default SubmitForm;
