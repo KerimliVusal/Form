09426f447593e28c99f5adfa4058a2ecc9e3c59a
KerimliVusal <kerimlivusal98@gmail.com>
2023-10-06 10:27:56 +0400

    new updates for errors

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,10 +3,11 @@ import { Input } from 'antd';
 import { CheckCircleFilled, CloseCircleFilled } from '@ant-design/icons'; 
 import '../../App.css'
 
-const Inputs = ({error,onChange,value,mount,Mount,data}) => {
+const Inputs = ({error,onChange,value,mount,Mount,data,reset=false}) => {
     useEffect(()=>{
-        if(error){Mount(true)}
-    },[error])
+        if(error){
+            Mount(true)}
+    },[error,reset])
   return (
     <div className={`Input ${(mount||data )&&(!error? 'inputCorrect' :'inputError')}`}>
         <Input onChange={onChange} value={value}  className='custom-input-class' />
